207a0933a84d91326a7249602887ff975fa0a28a
APIVerve Team <hello@apiverve.com>
2025-02-21 00:43:06 -0600

    Added new API Endpoints

---
+++ BarcodeGeneratorQueryOptions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class BarcodeGeneratorQueryOptions {
+public string data { get; set; }
+public string type { get; set; }
+public string lineColor { get; set; }
+public string backgroundColor { get; set; }
+public boolean displayValue { get; set; }
+}
+
+}
